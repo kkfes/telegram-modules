fdf9ad64ae2027026166c5291186f29b156bd700
Human <74965405+kkfes@users.noreply.github.com>
2022-09-18 21:53:22 +0600

    Update txt.py

--- txt.py
+++ txt.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 @loader.tds
-class SpamMod(loader.Module):
+class TxtMod(loader.Module):
     """Annoys people really effectively"""
 
     strings = {
@@ -32,31 +32,7 @@ class SpamMod(loader.Module):
             else:
                 await utils.answer(message, self.strings("nice_number", message))
                 return
-        count = "0"
-        spam = (await message.get_reply_message()) if use_reply else message
-        spam.message = " ".join(args[0])
-        try:
-            count = int(count)
-        except ValueError:
-            await utils.answer(message, self.strings(".", message))
-            return
-        if count < 1:
-            await utils.answer(message, self.strings(".", message))
-            return
-        await message.delete()
-        if count > 20:
-            # Be kind to other people
-            sleepy = 2
-        else:
-            sleepy = 0
-        i = 0
-        size = 1 if sleepy else 100
-        while i < count:
-            await asyncio.gather(
-                *[message.respond(spam) for x in range(min(count, size))]
-            )
-            await asyncio.sleep(sleepy)
-            i += size
-        await self.allmodules.log(
-            "spam", group=message.to_id, data=spam.message + " (" + str(count) + ")"
-        )
+        txtq = (await message.get_reply_message()) if use_reply else message
+        txtq.message = " ".join(args[0])
+
+        await utils.answer(message, self.strings("much_spam", txtq))
