46636212692f9f295d1f4fd0aa8dd3ec86b83658
Human <74965405+kkfes@users.noreply.github.com>
2022-10-17 20:46:16 +0600

    Update kmod.py

--- kmod.py
+++ kmod.py
@@ -17,36 +17,54 @@ class KMod(loader.Module):
 	
 
 	async def zaraddcmd(self, message):
-		".zaradd ответом на сообщение ириса с заражением, доабвит в список заражений"
+		" ответом на сообщение ириса с заражением, добавит в список заражений (@is/name значение) БЕЗ К, писать 8700 т т.д"
 		infList = self.db.get("KMod", "infList")
 		timezone = "Europe/Kiev"
 		time = datetime.now(pytz.timezone(timezone)).strftime("%d.%m")
 		reply = await message.get_reply_message()
-		if not reply: 
-			return await utils.answer(message, 'Реплай должен быть на смс ириса "<b>...подверг заражению...</b>"')
-		elif not 'подверг заражению' in reply.text and not 'подвергла заражению' in reply.text:
-			return await utils.answer(message, 'Реплай должен быть на смс ириса "<b>...подверг заражению...</b>"')
-		else: #☣
-			text = reply.text
-			x = text.index('☣')+4
-			count = text[x:].split(' ', maxsplit=1)[0]
-			t = str(count)
-			t = t.replace(",", "." )
-			if t.endswith('k'):
-				tn = float(t[0:(len(t)-1)])
-				tn=tn*1000
-				t=str(tn)
-			x = text.index('user?id=') + 8
-			user = '@' + text[x:].split('"', maxsplit=1)[0]
-			infList[user] = [t, time]
-			self.db.set("KMod", "infList", infList)
-			await utils.answer(message, f"✅ Пользователь <code>{user}</code> добавлен в список заражений.\nЧисло: <code>{count}</code>\nДата: <b>{time}</b>")
+		args = utils.get_args_raw(message)
+		args_list = args.split(' ')
+		if len(args_list)==2:
+			try:
+				user, count = str(args_list[0]), float(args_list[1])
+			except:
+				await utils.answer(message, "❎ Данные были введены не корректно")
+				return
+
+			infList[user] = [str(count), time]
+			self.db.set("NumMod", "infList", infList)
+			await utils.answer(
+				message,
+				f"✅ Пользователь <code>{args_list[0]}</code> добавлен в список заражений!\n"
+				f"ℹ️ Число: <code>{count}</code>\n"
+				f"📅 Дата: <b>{time}</b>"
+			)
+		else:
+			if not reply: 
+				return await utils.answer(message, '❎ Реплай должен быть на смс ириса "<b>...подверг заражению...</b>"')
+			elif reply.sender_id != 707693258 and not 'подверг заражению' in reply.text:
+				return await utils.answer(message, '❎ Реплай должен быть на смс ириса "<b>...подверг заражению...</b>"')
+			else: #☣
+				text = reply.text
+				x = text.index('☣')+4
+				count = text[x:].split(' ', maxsplit=1)[0]
+				t = str(count)
+				t = t.replace(",", "." )
+				if t.endswith('k'):
+					tn = float(t[0:(len(t)-1)])
+					tn=tn*1000
+					t=str(tn)
+				x = text.index('user?id=') + 8
+				user = '@' + text[x:].split('"', maxsplit=1)[0]
+				infList[user] = [t, time]
+				self.db.set("KMod", "infList", infList)
+				await utils.answer(message, f"✅ Пользователь <code>{user}</code> добавлен в список заражений.\nℹ️ Число: <code>{count}</code>\n📅 Дата: <b>{time}</b>")
 
 
 	async def zarlistcmd(self, message):
 		"Список всех заражений"
 		infList = self.db.get("KMod", "infList")
-		sms = 'Список заражений:\n'
+		sms = '🌀 Список заражений:\n'
 		for key, value in infList.items():
 			sms+=f'<b>• <code>{key}</code> -- <code>{value[0]}</code> [<i>{value[1]}</i>]</b>\n'
 		await utils.answer(message, sms)
@@ -58,7 +76,7 @@ class KMod(loader.Module):
 		args_list = args.split(' ')
 		try:
 			user = infList[args_list[0]]
-			await utils.answer(message, f"<b>✅ <code>{args_list[0]}</code> -- {user[0]} [<i>{user[1]}</i>]</b>")
+			await utils.answer(message, f"<b>✅ Жертва <code>{args_list[0]}</code>:\n☣️{user[0]} био-опыта.\n📆 Дата: <i>{user[1]}</i></b>")
 		except:
 			await utils.answer(message, "❎ Данного пользователя нет в списке.")
 
@@ -79,7 +97,7 @@ class KMod(loader.Module):
 		"Ответом на биотоп/биотоп чата/биотоп корп"
 		infList = self.db.get("KMod", "infList")
 		reply = await message.get_reply_message()
-		txt = 'Вот все провереные жертвы\n'
+		txt = '<b>👮 Вот все провереные жертвы:</b>\n'
 		messag = reply.message.split('\n')
 		i = 1
 		for value in reply.entities:	
@@ -91,7 +109,7 @@ class KMod(loader.Module):
 				except:
 					user = None
 				if user!=None:
-					num = int(user[0])
+					num = float(user[0])
 					idx1 = messag[i].index("|")
 					idx2 = messag[i].index("|",idx1+1)
 					give = messag[i][idx1+1:idx2-6]
@@ -102,14 +120,18 @@ class KMod(loader.Module):
 						tn = float(t[0:(len(t)-1)])
 						tn=tn*1000
 						t=str(tn)
-					num1 = int(t)/10
+					num1 = float(t)/10
 					if num1 > num:
-						txt+=str(i)+'. '+text+' - ✅ +'+str((num1-num))+'\n'
+						nnnn = num1-num
+						if nnnn <= 20:
+							txt+=str(i)+'. '+text+' - ➖ <code>'+str((num1-num))+'</code>\n'
+						else:
+							txt+=str(i)+'. '+text+' - ✅ <code>+'+str((num1-num))+'</code>\n'
 					else:
-						txt+=str(i)+'. '+text+' - ❌ '+str((num1-num))+'\n'
+						txt+=str(i)+'. '+text+' - ❌ <code>'+str((num1-num))+'</code>\n'
 				else:
-					txt+=str(i)+'. Нет информаций о жертве\n'
+					txt+=str(i)+'. Нет информаций 😔\n'
 			except Exception as e:
-				txt+=str(i)+' '+e
+				txt+=str(i)+' '+str(e)+'\n'
 			i=i+1
 		return await utils.answer(message,txt)
