3db799f3c148121aa1e244970a12ff60f962ed8f
Human <74965405+kkfes@users.noreply.github.com>
2022-10-27 16:28:15 +0600

    Update txt.py

--- txt.py
+++ txt.py
@@ -53,15 +53,3 @@ class TxtMod(loader.Module):
         	if text!=None:
         		await utils.answer(message,text)
         
-    async def pingcmd(self, message):
-        """Test your userbot ping"""
-        start = time.perf_counter_ns()
-        message = await utils.answer(message, "<code>🐻 Nofin...</code>")
-
-        await utils.answer(
-            message,
-            "<emoji document_id=6321050180095313397>⏱</emoji> <b>Скорость отклика"
-            " Telegram:</b> <code>"+round((time.perf_counter_ns() - start) / 10**6, 3)+"</code> <b>ms</b>\n<emoji"
-            " document_id=5377371691078916778>😎</emoji> <b>Прошло с последней"
-            " перезагрузки: "+utils.formatted_uptime()+"</b>")
-        )
