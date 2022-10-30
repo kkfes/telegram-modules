08ae002bb9635d8cdda6739b20869ff028c65322
Human <74965405+kkfes@users.noreply.github.com>
2022-10-30 19:46:01 +0600

    Create vkid.py

---
+++ vkid.py
@@ -0,0 +1,31 @@
+# meta developer: @kkfes
+
+from .. import loader, utils
+import logging
+import asyncio
+from datetime import datetime, date, time
+import asyncio, pytz, re, telethon
+
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class VkidMod(loader.Module):
+    """Модуль для бота СНЮСОЕД"""
+
+    strings = {
+        "name": "Vkid",
+    }
+
+    async def client_ready(self):
+        self._me = await self._client.get_me()
+
+    async def watcher(self, message):
+        fr = str(message.from_id)
+        if fr == "5431891078":
+            text = message.message
+            if 'начало отпускать, пора вкинуться' in text:
+                en = message.entities
+                if str(en[0].user_id)==str(self._me.id):
+                    await message.respond("/vkid")
